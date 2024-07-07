77c86a33d24b6eb9ad3d605538554732c2c22057
Adi-Marian Mutu <adi.mutu@norabytes.com>
2024-07-08 01:08:47 +0300

    [NoraTypes-v0.0.8] Remove `IsClass` as it can collide with the `functions` signature

--- libs/nora-types/src/conditionals.d.ts
+++ libs/nora-types/src/conditionals.d.ts
@@ -2,6 +2,5 @@ import { Primitive, UnknownArray, UnknownRecord } from 'type-fest';
 
 export type IsPrimitive<T> = T extends Primitive ? true : false;
 export type IsFunction<T> = T extends (...args: any[]) => any ? true : false;
-export type IsClass<T> = new (...args: any[]) => T extends new (...args: any[]) => any ? true : false;
 export type IsArray<T> = T extends UnknownArray ? true : false;
 export type IsRecord<T> = T extends UnknownRecord ? true : false;
