8e6b8d502d83eb09f23922f7d0fc0386ed384628
Adi-Marian Mutu <adi.mutu@norabytes.com>
2024-07-08 17:57:30 +0300

    [NoraTypes-v0.0.9] Fix the `dot-notation` not working with Interfaces
    + Use the `Get` generic type from `type-fest` for the `from-dot-notation` wrapper

--- libs/nora-types/src/mappers/from-dot-notation.d.ts
+++ libs/nora-types/src/mappers/from-dot-notation.d.ts
@@ -1,20 +1,3 @@
-import { DotNotation } from './dot-notation';
+import { Get } from 'type-fest';
 
-/**
- * Can be used to infer the generic type from a {@link DotNotation} `Type`.
- */
-export type FromDotNotation<T, P extends string> = PathValue<T, Split<P, '.'>>;
-
-type Split<S extends string, D extends string> = string extends S
-  ? string[]
-  : S extends `${infer T}${D}${infer U}`
-  ? [T, ...Split<U, D>]
-  : [S];
-
-type PathValue<T, P extends string[]> = P extends [infer Key, ...infer Rest]
-  ? Key extends keyof T
-    ? Rest extends string[]
-      ? PathValue<T[Key & keyof T], Rest>
-      : T[Key & keyof T]
-    : never
-  : T;
+export type FromDotNotation<T, P extends string | readonly string[]> = Get<T, P>;
--- libs/nora-types/src/mappers/index.d.ts
+++ libs/nora-types/src/mappers/index.d.ts
@@ -1,3 +1,3 @@
 export type * from './deep-override';
-export type * from './dot-notation';
+export type * from './leaves-dot-notation';
 export type * from './from-dot-notation';
