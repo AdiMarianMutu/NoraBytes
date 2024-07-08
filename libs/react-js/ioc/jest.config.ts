219db48e04feba87e971c7ef0b3ba69535237662
Adi-Marian Mutu <adi.mutu@norabytes.com>
2024-07-08 03:43:58 +0300

    [IoC] Update the `jest coverage` dir

--- libs/react-js/ioc/jest.config.ts
+++ libs/react-js/ioc/jest.config.ts
@@ -8,5 +8,5 @@ export default {
     '^.+\\.tsx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/react-js/ioc',
+  coverageDirectory: './coverage',
 };
