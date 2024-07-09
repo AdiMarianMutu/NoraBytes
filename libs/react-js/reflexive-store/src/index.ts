456af99b8d8e6806989047ee02243f13a2567353
Adi-Marian Mutu <adi.mutu@norabytes.com>
2024-07-09 21:58:29 +0300

    [ReactJS-ReflexiveStore-v1.0.1] Export the `StoreReduceResult` type

--- libs/react-js/reflexive-store/src/index.ts
+++ libs/react-js/reflexive-store/src/index.ts
@@ -2,10 +2,17 @@ import type {
   StoreMap as ReflexiveStoreMap,
   StoreContext as ReflexiveStoreContext,
   InitStoreConfig as ReflexiveInitStoreConfig,
+  StoreReduceResult as ReflexiveStoreReduceResult,
   IReflexiveStore,
 } from './types';
 import { StoreContextBuilder as ReflexiveStoreContextBuilder } from './utils';
 
-export type { ReflexiveStoreMap, ReflexiveStoreContext, ReflexiveInitStoreConfig, IReflexiveStore };
+export type {
+  ReflexiveStoreMap,
+  ReflexiveStoreContext,
+  ReflexiveInitStoreConfig,
+  ReflexiveStoreReduceResult,
+  IReflexiveStore,
+};
 export * from './reflexive-store';
 export { ReflexiveStoreContextBuilder };
