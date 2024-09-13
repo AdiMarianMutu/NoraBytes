c4e5df5d87c08ac229cf5c89c4a3f421123fdeda
Adi-Marian Mutu <adi.mutu@norabytes.com>
2024-09-13 22:41:29 +0300

    Revert "[ReactJS-ReflexiveStore-v1.1.1] Fix double re-render caused by the `useEffectOnce` helper hook"
    The below commit changes introduces a critical bug where the store is being disposed without an unmount
    
    This reverts commit 761a382426b2d91996e2b745fc9200856a40462f.

--- libs/react-js/reflexive-store/src/utils/use-effect-once.ts
+++ libs/react-js/reflexive-store/src/utils/use-effect-once.ts
@@ -1,22 +1,32 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+// Credits: https://stackoverflow.com/a/74000921
 
 /** Custom {@link useEffect} hook which will be run once. _(In `StrictMode` as well)_ */
 export function useEffectOnce(effect: () => React.EffectCallback) {
-  const cleanUpFn = useRef<() => void>(() => {});
-  const ref = useRef(false);
+  const destroyFunc = useRef<React.EffectCallback>();
+  const effectCalled = useRef(false);
+  const renderAfterCalled = useRef(false);
+  const [, forceRerender] = useState(0);
+
+  if (effectCalled.current) renderAfterCalled.current = true;
 
   useEffect(() => {
-    if (!ref.current) {
-      cleanUpFn.current = effect();
+    // only execute the effect first time around
+    if (!effectCalled.current) {
+      destroyFunc.current = effect();
+      effectCalled.current = true;
     }
 
-    return () => {
-      if (!ref.current) {
-        ref.current = true;
-        return;
-      }
+    // this forces one render after the effect is run
+    forceRerender((x) => x + 1);
 
-      cleanUpFn.current();
+    return () => {
+      // if the comp didn't render since the useEffect was called,
+      // we know it's the dummy React cycle
+      if (!renderAfterCalled.current) return;
+
+      destroyFunc.current?.();
     };
-  }, [effect]);
+  }, []);
 }
